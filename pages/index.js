f059f3c0f4e93edf1602649ee19b1e6aaa321844
Mohamed Fquihi <mo.fquihi@gmail.com>
2023-01-04 21:35:12 +0100

    ajout de nerby et live anywhere

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import Image from 'next/image'
 import Banner from '../components/Banner';
 import Header from '../components/Header';
 import SmallCard from '../components/SmallCard';
+import MediumCard from '../components/MediumCard';
 
 
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardsData }) {
   exploreData
   return (
     <div className="">
@@ -27,16 +28,30 @@ export default function Home({ exploreData }) {
           <h2 className='text-3xl font-bold pb-5'>Explore Nearby</h2>
 
        {/*Pull some data from a server - API endpoints*/} 
-
+      <div className='grid grid-cols-1 sm:grid-cols-2
+      lg:grid-cols-3 xl:grid-cols-4'>
        {exploreData?.map(item => (
         <SmallCard
+         key={item.img}
          img={item.img}
-         distance={item.img}
          location={item.location} />
        ))}
-
+      </div>
 
        </section> 
+
+       <section>
+        <h2 className='text-3xl font-bold py-8'>Live Anywhere</h2>
+
+        <div className="flex space-x-3 overflow-scroll scrollbar-hide"> 
+
+        {cardsData?.map(({img, title}) => (
+          <MediumCard key={img} img={img} title={title} />
+        ))}
+
+        </div>
+      
+       </section>
       </main>
 
      
@@ -50,9 +65,16 @@ export async function getStaticProps() {
     (res) => res.json()
   );
 
+  const cardsData = await fetch("https://www.jsonkeeper.com/b/VHHT").
+  then((res) => 
+   res.json()
+   );
+
+
   return {
     props: {
-      exploreData
+      exploreData,
+      cardsData
     }
   }
 }
